82fbd3f1453e668641c8eff2644e5eeac4b9b22a
felipopaganin <felipo.paganin1b1a2024@gmail.com>
2026-06-07 20:10:25 -0300

    Terceiro commit

---
+++ src/data/exercicios.js
@@ -0,0 +1,17 @@
+export const exercicios = [
+  {
+    id: '1',
+    nome: 'Supino',
+    imagem: require('../assets/supino.jpg'),
+  },
+  {
+    id: '2',
+    nome: 'Agachamento',
+    imagem: require('../assets/agachamento.jpg'),
+  },
+  {
+    id: '3',
+    nome: 'Corrida',
+    imagem: require('../assets/corrida.jpg'),
+  },
+];
